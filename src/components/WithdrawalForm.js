5c7f9635df3ea4dcc3a5f2a332260a97ee90249b
d0ngwooK1m <dongwookim.dev@gmail.com>
2021-11-03 00:06:36 +0900

    (김동우) add: Permit.js 완료(접근제한) edit: 로그인 회원가입 비밀번호변경 탈퇴 react-hook-form 및 sweetalert2 적용

--- src/components/WithdrawalForm.js
+++ src/components/WithdrawalForm.js
@@ -1,22 +1,31 @@
+/* eslint-disable */
 import React from 'react';
 import styled from 'styled-components';
 import { useDispatch } from 'react-redux';
-import { Grid, Input, Button } from '../elements';
+// import { Grid, Input, Button } from '../elements';
+import { useForm } from 'react-hook-form';
+import { Grid } from '../elements';
 import { withdrawalToServer } from '../features/user/actions';
+import Swal from 'sweetalert2';
 
 const WithdrawalForm = () => {
   const dispatch = useDispatch();
   const [inputStatus, setInputStatus] = React.useState('');
-  const [pw, setPw] = React.useState('');
-  const [pwCheck, setPwCheck] = React.useState('');
+  // const [pw, setPw] = React.useState('');
+  // const [pwCheck, setPwCheck] = React.useState('');
 
   const handleClickRadioButton = (radioBtnName) => {
     setInputStatus(radioBtnName);
   };
 
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
   return (
     <Grid>
-      <p>회원탈퇴</p>
       <RadioWrapper>
         <label htmlFor="radio1">
           <input
@@ -63,34 +72,149 @@ const WithdrawalForm = () => {
           더 이상 프로젝트를 하지 않아요
         </label>
       </RadioWrapper>
-      <Input
-        type="password"
-        place="비밀번호를 입력해주세요"
-        _onChange={(e) => {
-          setPw(e.target.value);
-        }}
-      />
-      <Input
-        type="password"
-        place="비밀번호를 확인해주세요"
-        _onChange={(e) => {
-          setPwCheck(e.target.value);
-        }}
-      />
-      <Button
-        _onClick={() => {
-          console.log(pw, pwCheck);
-          const passwordInfo = {
-            pw,
-            pwCheck,
-          };
-          dispatch(withdrawalToServer(passwordInfo));
-        }}
+      <form
+        onSubmit={
+          handleSubmit((passwordInfo) => {
+            const swalWithBootstrapButtons = Swal.mixin({
+              customClass: {
+                confirmButton: 'btn btn-success',
+                cancelButton: 'btn btn-danger'
+              },
+              buttonsStyling: false
+            })
+            
+            swalWithBootstrapButtons.fire({
+              title: 'Are you sure?',
+              text: "You won't be able to revert this!",
+              icon: 'warning',
+              showCancelButton: true,
+              confirmButtonText: 'Yes, delete it!',
+              cancelButtonText: 'No, cancel!',
+              reverseButtons: true
+            }).then((result) => {
+              if (result.isConfirmed) {
+                // swalWithBootstrapButtons.fire(
+                //   'Deleted!',
+                //   'Your file has been deleted.',
+                //   'success'
+                // )
+                dispatch(withdrawalToServer(passwordInfo));
+              } else if (
+                /* Read more about handling dismissals below */
+                result.dismiss === Swal.DismissReason.cancel
+              ) {
+                swalWithBootstrapButtons.fire(
+                  'Cancelled',
+                  'Your imaginary file is safe :)',
+                  'error'
+                )
+                return;
+              }
+            })
+            // console.log(passwordInfo);
+            // dispatch(withdrawalToServer(passwordInfo));
+          })
+        }
       >
-        회원탈퇴
-      </Button>
+        <input
+          type="password"
+          placeholder="비밀번호를 입력해주세요"
+          {...register('pw', {
+            required: '비밀번호를 입력해주세요',
+            maxLength: {
+              value: 20,
+              message: '비밀번호는 최대 20자입니다',
+            },
+            minLength: {
+              value: 6,
+              message: '비밀번호는 최소 6자 이상입니다',
+            },
+          })}
+        />
+        {errors.pw && <p>{errors.pw.message}</p>}
+        <br />
+        <input type="submit"/>
+      </form>
     </Grid>
   );
+
+  // return (
+  //   <Grid>
+  //     <p>회원탈퇴</p>
+  //     <RadioWrapper>
+  //       <label htmlFor="radio1">
+  //         <input
+  //           type="radio"
+  //           id="radio1"
+  //           checked={inputStatus === 'radio1'}
+  //           onChange={() => {
+  //             handleClickRadioButton('radio1');
+  //           }}
+  //         />
+  //         사용빈도가 낮아요
+  //       </label>
+  //       <label htmlFor="radio2">
+  //         <input
+  //           type="radio"
+  //           id="radio2"
+  //           checked={inputStatus === 'radio2'}
+  //           onChange={() => {
+  //             handleClickRadioButton('radio2');
+  //           }}
+  //         />
+  //         이용이 불편해요
+  //       </label>
+  //       <label htmlFor="radio3">
+  //         <input
+  //           type="radio"
+  //           id="radio3"
+  //           checked={inputStatus === 'radio3'}
+  //           onChange={() => {
+  //             handleClickRadioButton('radio3');
+  //           }}
+  //         />
+  //         기록을 삭제하고 싶어요
+  //       </label>
+  //       <label htmlFor="radio4">
+  //         <input
+  //           type="radio"
+  //           id="radio4"
+  //           checked={inputStatus === 'radio4'}
+  //           onChange={() => {
+  //             handleClickRadioButton('radio4');
+  //           }}
+  //         />
+  //         더 이상 프로젝트를 하지 않아요
+  //       </label>
+  //     </RadioWrapper>
+  //     <Input
+  //       type="password"
+  //       place="비밀번호를 입력해주세요"
+  //       _onChange={(e) => {
+  //         setPw(e.target.value);
+  //       }}
+  //     />
+  //     <Input
+  //       type="password"
+  //       place="비밀번호를 확인해주세요"
+  //       _onChange={(e) => {
+  //         setPwCheck(e.target.value);
+  //       }}
+  //     />
+  //     <Button
+  //       _onClick={() => {
+  //         console.log(pw, pwCheck);
+  //         const passwordInfo = {
+  //           pw,
+  //           pwCheck,
+  //         };
+  //         dispatch(withdrawalToServer(passwordInfo));
+  //       }}
+  //     >
+  //       회원탈퇴
+  //     </Button>
+  //   </Grid>
+  // );
 };
 
 const RadioWrapper = styled.div`
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,44 +1,115 @@
+/* eslint-disable */
 import React from 'react';
 import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router';
+import { useForm } from 'react-hook-form';
 import { loginToServer } from '../features/user/actions';
-import { Grid, Input, Button } from '../elements';
+// import { Grid, Input, Button } from '../elements';
 
 const Login = () => {
-  const dispatch = useDispatch();
-  const [email, setEmail] = React.useState('');
-  const [pw, setPw] = React.useState('');
+  // const [state, setState] = React.useState({
+  //   email: "",
+  //   pw: "",
+  // })
+  const history = useHistory();
 
-  const login = () => {
-    const loginInfo = {
-      email,
-      pw,
-    };
-    console.log(loginInfo);
-    dispatch(loginToServer(loginInfo));
-  };
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  // console.log(watch());
+
+  const dispatch = useDispatch();
+  // const [email, setEmail] = React.useState('');
+  // const [pw, setPw] = React.useState('');
+
+  // const login = () => {
+  //   const loginInfo = {
+  //     email,
+  //     pw,
+  //   };
+  //   console.log(loginInfo);
+  //   dispatch(loginToServer(loginInfo));
+  // };
 
   return (
-    <Grid>
-      <p>로그인</p>
-      <Input
-        type="email"
-        place="이메일을 입력해주세요"
-        _onChange={(e) => {
-          // console.log(e.target.value);
-          setEmail(e.target.value);
-        }}
+    <div>
+          <form
+      onSubmit={handleSubmit((loginInfo) => {
+        console.log(loginInfo);
+        dispatch(loginToServer(loginInfo));
+      })}
+    >
+      <input
+        type="text"
+        placeholder="이메일을 입력해주세요"
+        {...register('email', {
+          required: '이메일을 입력해주세요',
+          pattern: {
+            value: /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/,
+            message: '이메일 양식에 맞지 않습니다',
+          },
+        })}
       />
-      <Input
+      {errors.email && <p>{errors.email.message}</p>}
+      <br />
+      <input
         type="password"
-        place="비밀번호를 입력해주세요"
-        _onChange={(e) => {
-          // console.log(e.target.value);
-          setPw(e.target.value);
-        }}
+        placeholder="비밀번호를 입력해주세요"
+        {...register('pw', {
+          required: '비밀번호를 입력해주세요',
+          maxLength: {
+            value: 20,
+            message: '비밀번호는 최대 20자입니다',
+          },
+          minLength: {
+            value: 6,
+            message: '비밀번호는 최소 6자 이상입니다',
+          },
+        })}
       />
-      <Button _onClick={login}>로그인</Button>
-    </Grid>
+      {errors.pw && <p>{errors.pw.message}</p>}
+      <br />
+      <input type="submit" />
+      </form>
+      <span onClick={() => {
+        history.push('/signup');
+      }}>혹시 회원이 아니신가요?</span>
+    </div>
   );
+
+  // return (
+  //   <Grid>
+  //     <p>로그인</p>
+  //     <form onClick={handleSubmit(onSubmit)}>
+  //       <Input
+  //         type="email"
+  //         place="이메일을 입력해주세요"
+  //         // _onChange={(e) => {
+  //         //   // console.log(e.target.value);
+  //         //   setEmail(e.target.value);
+  //         // }}
+  //         {...register('email', { pattern: /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/i})}
+  //       />
+  //       {/* {errors.password && <span>{errors.password.message}</span>} */}
+  //       <Input
+  //         type="password"
+  //         place="비밀번호를 입력해주세요"
+  //         // _onChange={(e) => {
+  //         //   // console.log(e.target.value);
+  //         //   setPw(e.target.value);
+  //         // }}
+  //         {...register({
+  //           required: "You must specify a password",
+  //         })}
+  //       />
+  //       {/* <Button _onClick={login}>로그인</Button> */}
+  //       <input type="submit" />
+  //     </form>
+  //   </Grid>
+  // );
 };
 
 export default Login;
