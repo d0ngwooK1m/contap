dc8b14608cd988fe0a8d2ff481b7133f249016ca
d0ngwooK1m <dongwookim.dev@gmail.com>
2021-11-06 17:56:21 +0900

    (add) 김동우: 디자인 시스템 및 폰트 적용

---
+++ src/utils/systemDesign.js
@@ -0,0 +1,24 @@
+const ColorStyle = {
+  Gray500: '#F5F3F8',
+  Gray300: '#A09BAC',
+  Gray100: '#4D4759',
+  PrimaryMint: '#50FFB8',
+  PrimaryPurple: '#8C4DFF',
+  BackGround: '#0F0A1A',
+  BackGround100: '#141422',
+  BackGround300: '#1D1D22',
+  Error: '#ED4956',
+};
+
+const FontScale = {
+  Caption_14: '19px',
+  Body2_16: '22px',
+  Body1_20: '26px',
+  Header_24: '32px',
+  Header_32: '42px',
+};
+
+const FontFamily = `-apple-system, BlinkMacSystemFont, 'Apple SD Gothic Neo',
+Pretendard, Roboto, 'Noto Sans KR', 'Segoe UI', 'Malgun Gothic', sans-serif`;
+
+export { ColorStyle, FontScale, FontFamily };
