a212412687800bc2ce3e4f97c19eb9ca457ac514
aronglee <arong16@naver.com>
2021-11-09 04:20:45 +0900

    (이아영) edit : 카드 뒷면 추가,삭제 리덕스 수정, style : 마이페이지 CSS 수정
    (이아영) edit : 카드 뒷면 추가,삭제 리덕스 수정, style : 마이페이지 CSS 수정

--- src/pages/Mypage.js
+++ src/pages/Mypage.js
@@ -2,49 +2,53 @@ import React from 'react';
 import styled from 'styled-components';
 
 import { useDispatch, useSelector } from 'react-redux';
-import { loadMyCardDB } from '../features/cards/actions';
+import { loadMyCardDB, isSuccess } from '../features/cards/actions';
+// import { history } from '../features/configureStore';
 
 import CardProfile from '../components/CardProfile';
 import CardAdd from '../components/CardAdd';
 import CardBackWrite from '../components/CardBackWrite';
 
-import { Grid, Text } from '../elements';
+import { FontFamily, FontScale, ColorStyle } from '../utils/systemDesign';
+import { Grid } from '../elements';
 
 const Mypage = () => {
   const dispatch = useDispatch();
   const cardCount = useSelector((state) => state.cards.allIds);
 
-  const [click, setClick] = React.useState(false);
-
-  const getClick = (c) => {
-    setClick(c);
-  };
+  const handleClick = useSelector((state) => state.cards.isSuccess);
+  console.log(handleClick);
+  // const closeClick = () => {
+  //   setClick(false);
+  // };
 
   React.useEffect(() => {
     dispatch(loadMyCardDB());
   }, []);
 
-  if (click) {
+  if (handleClick) {
     return (
       <Grid>
         <Grid>
           <CardProfile />
         </Grid>
-        <Div>
-          <Text>나의 프로젝트 {cardCount.length}</Text>
-          <Btn
-            onClick={() => {
-              setClick(!click);
-            }}
-          >
-            프로젝트 추가하기
-          </Btn>
-        </Div>
-        <Grid>
-          <CardBackWrite getClick={getClick} />
-        </Grid>
-        <Grid>
-          <CardAdd />
+        <Grid width="100%" height="100%" bg="#0F0a1A">
+          <Div>
+            <TitleText>나의 프로젝트 {cardCount.length}</TitleText>
+            <TextBtn
+              onClick={() => {
+                dispatch(isSuccess(!handleClick));
+              }}
+            >
+              + 프로젝트 추가하기
+            </TextBtn>
+          </Div>
+          <Grid>
+            <CardBackWrite />
+          </Grid>
+          <Grid>
+            <CardAdd />
+          </Grid>
         </Grid>
       </Grid>
     );
@@ -55,19 +59,22 @@ const Mypage = () => {
       <Grid>
         <CardProfile />
       </Grid>
-      <Div>
-        <Text>나의 프로젝트 {cardCount.length}</Text>
-        <Btn
-          onClick={() => {
-            setClick(!click);
-            // history.push('/write');
-          }}
-        >
-          프로젝트 추가하기
-        </Btn>
-      </Div>
-      <Grid>
-        <CardAdd />
+      <Grid width="100%" height="100%" bg="#0F0a1A">
+        <Div>
+          <TitleText>
+            나의 프로젝트 <Count>{cardCount.length}</Count>
+          </TitleText>
+          <TextBtn
+            onClick={() => {
+              dispatch(isSuccess(!handleClick));
+            }}
+          >
+            + 프로젝트 추가하기
+          </TextBtn>
+        </Div>
+        <Grid>
+          <CardAdd />
+        </Grid>
       </Grid>
     </Grid>
   );
@@ -77,15 +84,36 @@ export default Mypage;
 
 const Div = styled.div`
   width: 1110px;
-  margin: 0px auto;
   display: flex;
   align-items: center;
   justify-content: space-between;
-  margin-bottom: 90px;
+  margin: 0px auto;
 `;
 
-const Btn = styled.button`
-  width: 80px;
+const TitleText = styled.p`
+  font-size: ${FontScale.Header_24};
+  font-family: ${FontFamily};
+  margin: 80px 0px 52px 0px;
+  color: ${ColorStyle.Gray500};
+  font-weight: 700;
+`;
+
+const Count = styled.span`
+  font-size: ${FontScale.Header_24};
+  font-family: ${FontFamily};
+  // margin: 160px 0px 64px 0px;
+  color: ${ColorStyle.PrimaryPurple};
+  font-weight: 700;
+`;
+
+const TextBtn = styled.div`
+  width: 15%;
+  font-size: ${FontScale.Body1_20};
+  font-family: ${FontFamily};
+  color: ${ColorStyle.Gray500};
+  cursor: pointer;
+  margin: 80px 0px 52px 0px;
+  font-weight: 700;
   // position: fixed;
   // top: 16vh;
   // left: 47vw;
