9075a25428fa454250113442eff07f213ab03433
d0ngwooK1m <dongwookim.dev@gmail.com>
2021-11-09 22:08:15 +0900

    (김동우) add: Opacity 추가

--- src/utils/systemDesign.js
+++ src/utils/systemDesign.js
@@ -18,7 +18,32 @@ const FontScale = {
   Header_32: '32px',
 };
 
+// 사용법 +Opacity[num]
+const Opacity = {
+  100: 'FF',
+  95: 'F2',
+  90: 'E6',
+  85: 'D9',
+  80: 'CC',
+  75: 'BF',
+  70: 'B3',
+  65: 'A6',
+  60: '99',
+  55: '8C',
+  50: '80',
+  45: '73',
+  40: '66',
+  35: '59',
+  30: '4D',
+  25: '40',
+  20: '33',
+  15: '26',
+  10: '1A',
+  5: '0D',
+  0: '00',
+};
+
 const FontFamily = `-apple-system, BlinkMacSystemFont, 'Apple SD Gothic Neo',
 Pretendard, Roboto, 'Noto Sans KR', 'Segoe UI', 'Malgun Gothic', sans-serif`;
 
-export { ColorStyle, FontScale, FontFamily };
+export { ColorStyle, FontScale, FontFamily, Opacity };
