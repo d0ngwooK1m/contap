4cecabef1fa593dcdce5036fe5062260a62c0be7
Wooseok Han <wswj9608@naver.com>
2021-11-10 10:38:05 +0900

    (한우석) add :로그인 안하고 카드 앞면 클릭 시 Alert 추가, sweetalert2-react-content Lib 추가

--- src/components/CardModal.js
+++ src/components/CardModal.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Modal from '@mui/material/Modal';
-
 import { Grid } from '../elements';
 import Carousel from './Carousel';
 
