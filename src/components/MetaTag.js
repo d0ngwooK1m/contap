a39d2231be6508c4456bba9cf8d071d73dbf02b0
d0ngwooK1m <dongwookim.dev@gmail.com>
2021-11-28 01:51:52 +0900

    (김동우) edit: 메타태그 react-helmet-async 사용

--- src/components/MetaTag.js
+++ src/components/MetaTag.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import { Helmet } from 'react-helmet';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
 
 const MetaTag = () => {
   return (
-    <Helmet>
-      <meta property="og:title" content="Contap" />
-      <meta
-        property="og:description"
-        content="Just Tap! Contap에서는 함께 성장할 수 있어요"
-      />
-      <meta property="og:image" content="%PUBLIC_URL%/logo.png" />
-    </Helmet>
+    <HelmetProvider>
+      <Helmet>
+        <meta property="og:title" content="Contap" />
+        <meta
+          property="og:description"
+          content="Just Tap! Contap에서는 함께 성장할 수 있어요"
+        />
+        <meta property="og:image" content="%PUBLIC_URL%/logo.png" />
+      </Helmet>
+    </HelmetProvider>
   );
 };
 
